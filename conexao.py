8e1734c0a776ea55a67c5b3c55a5723065214466
Marlon Duarte <eletromarlon@gmail.com>
2022-06-20 19:04:17 -0300

    Adding fixes and refactoring on some lines

--- conexao.py
+++ conexao.py
@@ -1,19 +1,17 @@
-from multiprocessing import connection
 import cx_Oracle
 import psycopg
 import sqlite3
 import mysql.connector
 
-
 #cx_Oracle.init_oracle_client(lib_dir=r"C:\oracle\instantclient_21_3")
 
 connection = None
 
-def oracleConnection(username, password, dsn):
+def oracleConnection(username, password, dsn, query):
     saida = []
     connection = cx_Oracle.connect(username,password,dsn,encoding='UTF-8')
     cursor = connection.cursor()
-    for row in cursor.execute("SELECT ID_PEDIDO FROM SIHPB_CIVIL.PEDIDO FETCH FIRST 100 ROWS ONLY"):
+    for row in cursor.execute(query):
         saida.append(row[0])
     connection.commit()
     connection.close()
@@ -23,14 +21,14 @@ def mysqlConnection(host, dbname, user, password, consulta):
     connection = mysql.connector.connect(host=host, database=dbname, user=user, password=password)
     cursor = connection.cursor()
     cursor.execute(consulta)
-    print(cursor.fetchall())
+    return cursor.fetchall()
     connection.close()
 
 def sqLiteConnection(consulta):
     connection = sqlite3.Connection("teste.db")
     cursor = connection.cursor()
     cursor.execute(consulta)
-    print(cursor.fetchall())
+    return cursor.fetchall()
     # for row in cursor.execute(consulta):
     #     print(row)
     connection.close()
@@ -41,25 +39,6 @@ def postgresConnection(host, dbname, user, password, consulta):
     cursor.execute(consulta)
     # for record in cursor:
     #     print(record)
-    print(cursor.fetchall())
+    return (cursor.fetchall())
     connection.commit()
     connection.close()
-
-"""try:
-    connection = cx_Oracle.connect(
-        config.username,
-        config.password,
-        config.dsn,
-        encoding=config.encoding)
-
-    # show the version of the Oracle Database
-    print(connection.version)
-    cursor = connection.cursor
-except cx_Oracle.Error as error:
-    print(error)
-finally:
-    # release the connection
-    if connection:
-        connection.close()
-"""
-
--- index.py
+++ index.py
@@ -1,55 +1,256 @@
 from flask import Flask, jsonify, render_template, request
 import conexao
+
 app = Flask(__name__)
 
-#@app.route('/')
-#def root(): # funcao de retorno da rota
-#    return 'Hello World!'
+#variaveis globais para alocação de dados dos bancos do usuário
+bdData1 = None
+bdData2 = None
+bdOut1 = []
+bdOut2 = []
 
-@app.route('/') # nome da rota
-def teste():
+# Rota inicial, responsável por exibir a pagina inicial
+@app.route('/') 
+def home():
+    global bdData1
+    global bdData2
+    global bdOut1
+    global bdOut2
+
+    bdData1 = None
+    bdData2 = None
+    bdOut1 = []
+    bdOut2 = []
+
     return (
        render_template('index.html')
     )
 
-@app.route('/graph', methods=['GET', 'POST']) # nome da rota
-def postTest():
-    bdOut = []
-    bdData = {}
+#Rora de primeira conexao com banco, as variaveis globais visam manter as informações para outras consultas desejadas pelo usuário
+@app.route('/conexao1', methods=['GET', 'POST']) # nome da rota
+def connectionBd1():
+    bdOut1 = []
+    global bdData1
+    global bdData2
+
     if request.method == 'POST':
-        bdData = {
+        bdData1 = {
             "user": request.form.get('user1'),
             "password": request.form.get('password1'),
             "id": request.form.get('id1'),
             "port": request.form.get('port1'),
             "bdType": request.form.get('bd1')
-        }
-    
-    print(bdData["user"])
+        } 
+        if (not bdData2):
+            bdData2 = {
+                "user": "",
+                "password": "",
+                "id": "",
+                "port": "",
+                "bdType": ""
+            }
 
-    try:
-        if bdData["bdType"] == 'Oracle':
-            bdOut = conexao.oracleConnection(bdData["user"], bdData["password"], bdData["id"])
-    except:
+    if (True):
+        try:
+            if bdData1["bdType"] == 'Oracle':
+                bdOut1 = conexao.oracleConnection(bdData1["user"], bdData1["password"], bdData1["id"], "SELECT 1") 
+            if bdData1["bdType"] == 'MySQL':
+                bdOut1 = conexao.mysqlConnection(bdData1["port"], bdData1["id"], bdData1["user"], bdData1["password"], "SELECT 1")
+            if bdData1["bdType"] == 'PostgreSQL':
+                bdOut1 = conexao.postgresConnection(bdData1["port"], bdData1["id"], bdData1["user"], bdData1["password"], "SELECT 1")
+            if bdData1["bdType"] == 'SQLite':
+                bdOut1 = conexao.sqLiteConnection("SELECT 1")
+        except:
+            return render_template(
+                'index.html',
+                user1 = "CONNECTION ERROR",
+                password1 = "CONNECTION ERROR",
+                id1 = "CONNECTION ERROR",
+                port1 = "CONNECTION ERROR",
+                bd1 = "CONNECTION ERROR",
+                consulta1 = bdOut1   
+            )
+
         return render_template(
             'index.html',
-            user = "CONNECTION ERROR",
-            password = "CONNECTION ERROR",
-            id = "CONNECTION ERROR",
-            port = "CONNECTION ERROR",
-            bd = "CONNECTION ERROR",
-            consulta = bdOut   
+            user1 = bdData1["user"],
+            password1 = bdData1["password"],
+            id1 = bdData1["id"],
+            port1 = bdData1["port"],
+            bd1 = bdData1["bdType"],
+            user2 = bdData2["user"],
+            password2 = bdData2["password"],
+            id2 = bdData2["id"],
+            port2 = bdData2["port"],
+            bd2 = bdData2["bdType"],
+            consulta1 = bdOut1  
         )
-        
-    return render_template(
-        'index.html',
-        user = bdData["user"],
-        password = bdData["password"],
-        id = bdData["id"],
-        port = bdData["port"],
-        bd = bdData["bdType"],
-        consulta = bdOut   
-    )
+
+#Rora de segunda conexao, as variaveis globais visam manter as informações para outras consultas desejadas pelo usuário
+@app.route('/conexao2', methods=['GET', 'POST']) # nome da rota
+def connectionBd2():
+    bdOut2 = []
+    global bdData1
+    global bdData2
+
+    if request.method == 'POST':
+        bdData2 = {
+            "user": request.form.get('user2'),
+            "password": request.form.get('password2'),
+            "id": request.form.get('id2'),
+            "port": request.form.get('port2'),
+            "bdType": request.form.get('bd2')
+        }
+        if (not bdData1):
+            bdData1 = {
+                "user": "",
+                "password": "",
+                "id": "",
+                "port": "",
+                "bdType": ""
+            }
+
+
+    if (True):
+        try:
+            if bdData2["bdType"] == 'Oracle':
+                bdOut2 = conexao.oracleConnection(bdData2["user"], bdData2["password"], bdData["id"], "SELECT 1") 
+            if bdData2["bdType"] == 'MySQL':
+                bdOut2 = conexao.mysqlConnection(bdData2["port"], bdData2["id"], bdData2["user"], bdData2["password"], "SELECT 1")
+            if bdData2["bdType"] == 'PostgreSQL':
+                bdOut2 = conexao.postgresConnection(bdData2["port"], bdData2["id"], bdData2["user"], bdData2["password"], "SELECT 1")
+            if bdData2["bdType"] == 'SQLite':
+                bdOut2 = conexao.sqLiteConnection("SELECT 1")
+            
+            
+        except:
+            return render_template(
+                'index.html',
+                user2 = "CONNECTION ERROR",
+                password2 = "CONNECTION ERROR",
+                id2 = "CONNECTION ERROR",
+                port2 = "CONNECTION ERROR",
+                bd2 = "CONNECTION ERROR",
+                consulta2 = bdOut2   
+            )
+
+        return render_template(
+            'index.html',
+            user1 = bdData1["user"],
+            password1 = bdData1["password"],
+            id1 = bdData1["id"],
+            port1 = bdData1["port"],
+            bd1 = bdData1["bdType"],
+            user2 = bdData2["user"],
+            password2 = bdData2["password"],
+            id2 = bdData2["id"],
+            port2 = bdData2["port"],
+            bd2 = bdData2["bdType"],
+            consulta2 = bdOut2   
+        )
+
+@app.route('/consultasql', methods=['GET', 'POST'])
+def consultasql():
+    global bdOut1
+    global bdOut2
+    global bdData1
+    global bdData2
+
+    consulta1 = request.form.get('sql1')
+    consulta2 = request.form.get('sql2')
+
+    print("bdData1", bdData1)
+    print("bdData2", bdData2)
+    print("sql1", consulta1)
+    print("sql2", consulta2)
+
+    if request.method == 'POST':
+        if (bdData1 != None and consulta1 != None):
+            try:
+                if bdData1["bdType"] == 'Oracle':
+                    bdOut1 = conexao.oracleConnection(bdData1["user"], bdData1["password"], bdData1["id"], consulta1) 
+                if bdData1["bdType"] == 'MySQL':
+                    bdOut1 = conexao.mysqlConnection(bdData1["port"], bdData1["id"], bdData1["user"], bdData1["password"], consulta1)
+                if bdData1["bdType"] == 'PostgreSQL':
+                    bdOut1 = conexao.postgresConnection(bdData1["port"], bdData1["id"], bdData1["user"], bdData1["password"], consulta1)
+                if bdData1["bdType"] == 'SQLite':
+                    bdOut1 = conexao.sqLiteConnection(consulta1)
+            except:
+                return render_template(
+                    'index.html',
+                    user1 = bdData1["user"],
+                    password1 = bdData1["password"],
+                    id1 = bdData1["id"],
+                    port1 = bdData1["port"],
+                    bd1 = bdData1["bdType"],
+                    user2 = bdData2["user"],
+                    password2 = bdData2["password"],
+                    id2 = bdData2["id"],
+                    port2 = bdData2["port"],
+                    bd2 = bdData2["bdType"],
+                    consulta1 = "ALGUM ERRO OCORREU NA CONSULTA"   
+               )
+            return render_template(
+                    'index.html',
+                    user1 = bdData1["user"],
+                    password1 = bdData1["password"],
+                    id1 = bdData1["id"],
+                    port1 = bdData1["port"],
+                    bd1 = bdData1["bdType"],
+                    user2 = bdData2["user"],
+                    password2 = bdData2["password"],
+                    id2 = bdData2["id"],
+                    port2 = bdData2["port"],
+                    bd2 = bdData2["bdType"],
+                    consulta1 = bdOut1   
+               )
+        else:
+            if (bdData2 != None and consulta2 != None):
+                try:
+                    if bdData2["bdType"] == 'Oracle':
+                        bdOut2 = conexao.oracleConnection(bdData2["user"], bdData2["password"], bdData["id"], consulta2) 
+                    if bdData2["bdType"] == 'MySQL':
+                        bdOut2 = conexao.mysqlConnection(bdData2["port"], bdData2["id"], bdData2["user"], bdData2["password"], consulta2)
+                    if bdData2["bdType"] == 'PostgreSQL':
+                        bdOut2 = conexao.postgresConnection(bdData2["port"], bdData2["id"], bdData2["user"], bdData2["password"], consulta2)
+                    if bdData2["bdType"] == 'SQLite':
+                        bdOut2 = conexao.sqLiteConnection("SELECT 1")
+                except:
+                    return render_template(
+                        'index.html',
+                        user1 = bdData1["user"],
+                        password1 = bdData1["password"],
+                        id1 = bdData1["id"],
+                        port1 = bdData1["port"],
+                        bd1 = bdData1["bdType"],
+                        user2 = bdData2["user"],
+                        password2 = bdData2["password"],
+                        id2 = bdData2["id"],
+                        port2 = bdData2["port"],
+                        bd2 = bdData2["bdType"],
+                        consulta1 = "ALGUM ERRO OCORREU NA CONSULTA"   
+                    )
+            else:
+                return render_template(
+                    'index.html',
+                    erro = "Erro de consulta ou banco desconectado. Clique em 'Limpar Tela' depois conecte algum banco para refazer a query"
+                )
+            return render_template(
+                'index.html',
+                user1 = bdData1["user"],
+                password1 = bdData1["password"],
+                id1 = bdData1["id"],
+                port1 = bdData1["port"],
+                bd1 = bdData1["bdType"],
+                user2 = bdData2["user"],
+                password2 = bdData2["password"],
+                id2 = bdData2["id"],
+                port2 = bdData2["port"],
+                bd2 = bdData2["bdType"],
+                consulta2 = bdOut2
+            )
+    else:
+        return render_template('index.html')
 
 @app.route('/generator', methods=['GET', 'POST']) # nome da rota
 def getGenerator():
@@ -64,7 +265,28 @@ def getGenerator():
             return render_template('index.html')
         else:
             return render_template('index.html')
-  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 '''    
 @app.route('/pessoas/<string:nome>/<string:cidade>')
 def pessoa(nome, cidade):
